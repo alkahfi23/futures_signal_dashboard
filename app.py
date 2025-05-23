af7dd54e3f31ff67d43525746976ad91a8aaef3c
alkahfi23 <58243832+alkahfi23@users.noreply.github.com>
2025-05-23 18:11:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ LIMIT = 100
 REFRESH_INTERVAL = 55
 account_balance = 18
 risk_pct = 5
-leverage = 50
-MIN_QTY = 0.001
+leverage = 100
+MIN_QTY = 0.0001
 
 # ====== Helpers ======
 @st.cache_data(ttl=REFRESH_INTERVAL)
